87e07a6e8aba66378d1cd62396d92701092e9cf9
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-09-24 23:29:43 +0530

    fix: simplify MongoDB connection options by removing deprecated parameters

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ async function connectDB(uri) {
     if (!uri) throw new Error('MONGODB_URI not provided');
 
     try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(uri);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection failed:', err.message);
