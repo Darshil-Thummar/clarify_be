fa38a75ca2842fd33e63dd08998d3720973fe29f
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-09-24 23:23:05 +0530

    feat: implement message storage and retrieval, update sessionId handling to use MongoDB ObjectId

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const createToken = (user) => {
     return jwt.sign({id: user._id}, process.env.JWT_SECRET, {
